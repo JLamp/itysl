62d9e881ee7311a360e5e52178fef208819d3469
JLamp <jlampron@wistia.com>
2021-08-29 11:04:28 -0400

    change background

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,13 +5,11 @@ import logo from "../public/images/logo.svg";
 export function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>
-        <Image
-          src={logo}
-          priority={true}
-          alt="I Think You Should Leave Database"
-        />
-      </div>
+      <Image
+        src={logo}
+        priority={true}
+        alt="I Think You Should Leave Database"
+      />
     </div>
   );
 }
